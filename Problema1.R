63766fe14dc4cece53841c5c2b67c4fa9b78b06b
nil.puig.biosca <nil.puig.biosca@auleseebe.upc.edu>
2024-10-03 11:29:52 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,11 @@
 #Codigo para problema 1
 
+#multiple Rsquared=coeficient de determinació(1)
+#coeficient de correlació=sqrt(coeficient de determinació)(2)
+#manera diferent de demanar el pendent de assets=0.06576 (3)
+m<-0.0658
+b<-11.798
+x<-647.1
+
+m*x+b
+#a dalt pregunta el valor de y per x=647.1(4)
